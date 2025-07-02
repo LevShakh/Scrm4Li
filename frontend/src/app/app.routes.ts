4f5cc4f5bfbf7ffda72e7ce250bac945af9b7629
Lev Shumara <shumara2001@gmail.com>
2025-07-02 23:26:01 +0300

    Angular how works verstanden

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/auth/home/home';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register},
+    { path: '**', redirectTo: '' }
+];
